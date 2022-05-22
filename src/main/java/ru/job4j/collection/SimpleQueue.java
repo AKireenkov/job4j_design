e40d6b3a518f50faed4f07916af5d4c699bbef48
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-22 16:02:09 +0300

    5. Очередь на двух стеках [#160 #301944]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
@@ -15,6 +17,9 @@ public class SimpleQueue<T> {
             }
         }
         lengthOut--;
+        if (lengthOut == -1) {
+            throw new NoSuchElementException();
+        }
         return out.pop();
     }
 
