1c10d6db287e8f05cb8c4ac286b4bc34d61a33ca
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-23 18:35:40 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/eqhc/User.java
+++ src/main/java/ru/job4j/eqhc/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.eqhc;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -13,5 +16,15 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Andrey", 1, new GregorianCalendar(1990, Calendar.SEPTEMBER, 12));
+        User user2 = new User("Andrey", 1, new GregorianCalendar(1990, Calendar.SEPTEMBER, 12));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map.get(user1));
+        System.out.println(map.get(user2));
     }
 }
