dec5d2413362bee6ec0b985f9f6d8192df38f19c
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-28 23:59:25 +0300

    3.0. Тестирование IO [#173905 #306490]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -18,21 +18,21 @@ public class AnalizyTest {
         File source = folder.newFile("statusWithOneError.txt");
         File target = folder.newFile("statusWithOneError.csv");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01");
-            out.println("500 10:57:01");
-            out.println("400 10:58:01");
-            out.println("500 10:59:01");
-            out.println("400 11:01:02");
-            out.println("200 11:02:02");
+            out.println("""
+                    200 10:56:01
+                    500 10:57:01
+                    400 10:58:01
+                    500 10:59:01
+                    400 11:01:02
+                    200 11:02:02
+                    """);
         }
         Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            in.lines().forEach(l -> rsl.append(l).append(System.lineSeparator()));
+            in.lines().forEach(rsl::append);
         }
-        assertThat(rsl.toString(), is("""
-                10:57:01;11:02:02;\r
-                """));
+        assertThat(rsl.toString(), is("10:57:01;11:02:02;"));
     }
 
     @Test
@@ -40,12 +40,14 @@ public class AnalizyTest {
         File source = folder.newFile("statusWithTwoError.txt");
         File target = folder.newFile("statusWithTwoError.csv");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01");
-            out.println("500 10:57:01");
-            out.println("400 10:58:01");
-            out.println("200 10:59:01");
-            out.println("500 11:01:02");
-            out.println("200 11:02:02");
+            out.println("""
+                    200 10:56:01
+                    500 10:57:01
+                    400 10:58:01
+                    200 10:59:01
+                    500 11:01:02
+                    200 11:02:02
+                    """);
         }
         Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         StringBuilder rsl = new StringBuilder();
