e86f94dcd4039286f2a466d04fed4c7cd52d408a
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-31 18:09:49 +0300

    2. Анализ доступности сервера. [#859 #306483]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,23 +1,20 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.List;
-import java.util.ListIterator;
 
 public class Analizy {
     public static void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            List<String> lines = read.lines().toList();
-            ListIterator<String> it = lines.listIterator();
-            while (it.hasNext()) {
-                String cur = it.next();
-                if (cur.contains("400 ") || cur.contains("500 ")) {
-                    out.print(getTimeLog(cur) + ";");
-                    while (cur.contains("400 ") || cur.contains("500 ")) {
-                        cur = it.next();
-                    }
-                    out.println(getTimeLog(cur) + ";");
+            boolean error = false;
+            for (String s : read.lines().toList()) {
+                boolean haveCode = s.contains("400 ") || s.contains("500 ");
+                if (!error && haveCode) {
+                    out.print(getTimeLog(s) + ";");
+                    error = true;
+                } else if (error && !haveCode) {
+                    error = false;
+                    out.print(getTimeLog(s) + ";");
                 }
             }
         } catch (IOException ioe) {
@@ -25,7 +22,7 @@ public class Analizy {
         }
     }
 
-    public static String getTimeLog(String line) {
+    private static String getTimeLog(String line) {
         String[] log = line.split(" ");
         return log[1];
     }
