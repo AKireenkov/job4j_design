5d8c21a770beb9982b559ac6055520dc0e318026
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-31 22:58:26 +0300

    2. Анализ доступности сервера. [#859 #306483]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,14 +7,15 @@ public class Analizy {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             boolean error = false;
-            for (String s : read.lines().toList()) {
+            while (read.ready()) {
+                String s = read.readLine();
                 boolean haveCode = s.contains("400 ") || s.contains("500 ");
                 if (!error && haveCode) {
                     out.print(getTimeLog(s) + ";");
                     error = true;
                 } else if (error && !haveCode) {
                     error = false;
-                    out.print(getTimeLog(s) + ";");
+                    out.println(getTimeLog(s) + ";");
                 }
             }
         } catch (IOException ioe) {
