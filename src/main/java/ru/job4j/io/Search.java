2940565c226bd53c419b3e61934424f0a8b78e2c
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-06-02 14:08:07 +0300

    5. Валидация параметров запуска. [#246865 #308046]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -6,10 +6,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        checkValidArgs(args.length);
+        checkValidArgs(args);
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
@@ -20,9 +22,21 @@ public class Search {
         return searcher.getPaths();
     }
 
-    private static void checkValidArgs(int length) {
-        if (length != 2) {
+    private static void checkValidArgs(String[] args) {
+        if (args.length != 2) {
             throw new IllegalArgumentException("check the number of arguments passed");
+        }
+
+        Pattern path = Pattern.compile("([A-Z|a-z]:\\\\[^*|\"<>?\\n]*)|(\\\\\\\\.*?\\\\.*)|(\\.)");
+        Matcher matcherPath = path.matcher(args[0]);
+        if (!matcherPath.find()) {
+            throw new IllegalArgumentException("the path to the directory is not valid");
+        }
+
+        Pattern extension = Pattern.compile("\\.[a-zA-Z]{1,}");
+        Matcher matcherExtension = extension.matcher(args[1]);
+        if (!matcherExtension.find()) {
+            throw new IllegalArgumentException("the format does not match pattern");
         }
     }
 }
