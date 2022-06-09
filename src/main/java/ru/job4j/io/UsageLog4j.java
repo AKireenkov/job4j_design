1105f848130369e961b33c65510658ffb18a5c29
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-06-09 12:32:50 +0300

    3. Slf4j - вывод переменных. [#268852 #310678]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -9,6 +9,7 @@ public class UsageLog4j {
 
     public static void main(String[] args) {
         String name = "Andrey Kireenkov";
+        byte monthsOfTraining = 3;
         int age = 33;
         boolean maleGender = true;
         float height = 192.5F;
@@ -16,7 +17,7 @@ public class UsageLog4j {
         char gender = 'M';
         short shoeSize = 43;
         long clothingSize = 33;
-        LOG.debug("User info name : {}, age : {}, maleGender : {}, height : {}, weight : {}, gender : {}, shoeSize : {}, clothingSize : {}",
-                name, age, maleGender, height, weight, gender, shoeSize, clothingSize);
+        LOG.debug("User info name : {}, age : {}, maleGender : {}, height : {}, weight : {}, gender : {}, shoeSize : {}, clothingSize : {}, monthsOfTraining : {}",
+                name, age, maleGender, height, weight, gender, shoeSize, clothingSize, monthsOfTraining);
     }
 }
