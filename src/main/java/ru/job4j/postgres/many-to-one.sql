07f953df9f28ddd7a1f169a95531ee144eaf1a41
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-06-13 22:53:32 +0300

    0.1. Связи между таблицами [#373615 #311701]

--- src/main/java/ru/job4j/postgres/many-to-one.sql
+++ src/main/java/ru/job4j/postgres/many-to-one.sql
@@ -1,16 +1,16 @@
 create table passenger(
 id serial primary key,
 	name char(255)
+	airplane_id int references airplane(id)
 );
 
 create table airplane(
 id serial primary key,
-	seat_number char(255),
-	passenger_id int references passenger(id)
+	name char(255),
 );
 
-insert into passenger (name) values ('Andrey');
-insert into airplane (seat_number, passenger_id) values ('1C', 1);
+insert into airplane (name) values ('Pobeda');
+insert into passenger (name, airplane_id) values ('Andrey', 1);
 
-select *  from passenger;
-select * from airplane where id in (select id from passenger);
+select * from airplane ;
+select *  from passenger where id in (select id from passenger);
