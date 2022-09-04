476cc774e5c672896f73f394f82e28fc49b42242
andreykireenkov <kireeshka32@gmail.com>
2022-09-04 21:17:40 +0300

    0. Принцип единственной ответственности [#4913 #338580]

---
+++ src/main/java/ru/job4j/srp/User.java
@@ -0,0 +1,45 @@
+package ru.job4j.srp;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private int age;
+    private String role;
+
+    private List<User> users;
+
+    public User(String name, int age, String role) {
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void add(User user) {
+        users.add(user);
+    }
+}
