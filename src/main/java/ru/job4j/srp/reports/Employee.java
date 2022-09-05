f138b45167ff72a33c48144848116398e973c9e3
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-09-05 13:11:41 +0300

    1. Отчеты. [#850 #339754]

--- src/main/java/ru/job4j/srp/reports/Employee.java
+++ src/main/java/ru/job4j/srp/reports/Employee.java
@@ -67,11 +67,11 @@ public class Employee {
 
     @Override
     public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", hired=" + hired +
-                ", fired=" + fired +
-                ", salary=" + salary +
-                '}';
+        return "Employee{"
+                + "name='" + name + '\''
+                + ", hired=" + hired
+                + ", fired=" + fired
+                + ", salary=" + salary
+                + '}';
     }
 }
