52b4ded12e7232116b26284b74431a4c7c8cca4f
andreykireenkov <kireeshka32@gmail.com>
2022-09-09 16:35:09 +0300

    0. Принцип подстановки Лисков. [#4915]

---
+++ src/main/java/ru/job4j/ood/lsp/Audi.java
@@ -0,0 +1,43 @@
+package ru.job4j.ood.lsp;
+
+/**
+ * Класс для демонстрации нарушения принципа LSP.
+ *
+ * @author Andrey Kireenkov
+ * @version 1.0
+ * @since 09.09.2022
+ */
+public class Audi extends Car {
+    public Audi(int wheel, String color) {
+        super(wheel, color);
+    }
+
+    /**
+     * Метод нарушает принцип,
+     * т.к. условие в родительском классе Car, которое выбрасывает исключение - тут не реализовано.
+     *
+     * @param color цвет автомобиля.
+     * @return стоимость, в зависимости от цвета автомобиля.
+     */
+    @Override
+    public int cost(String color) {
+        int cost = 777;
+        if ("A8".equals(color)
+                || "A4".equals(color)
+                || "TT".equals(color)) {
+            cost = 1000;
+        }
+        return cost;
+    }
+
+    /**
+     * Метод нарушает принцип, т.к. тут реализована лишняя проверка на возраст водителя.
+     *
+     * @param driver объект водитель.
+     * @return true если водитель имеет право на вождение, иначе false.
+     */
+    @Override
+    public boolean canDrive(Driver driver) {
+        return super.canDrive(driver) && driver.getAge() > 21;
+    }
+}
---
+++ src/main/java/ru/job4j/ood/lsp/Driver.java
@@ -0,0 +1,21 @@
+package ru.job4j.ood.lsp;
+
+public class Driver {
+    private String name;
+    private int age;
+    private boolean driverLicense;
+
+    public Driver(String name, int age, boolean driverLicense) {
+        this.name = name;
+        this.age = age;
+        this.driverLicense = driverLicense;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isDriverLicense() {
+        return driverLicense;
+    }
+}
