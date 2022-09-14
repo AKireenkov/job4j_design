fc3be4bf9ddb776bc8e969b1be2dd7333b0d4599
andreykireenkov <kireeshka32@gmail.com>
2022-09-14 21:33:39 +0300

    2. Парковка машин [#853] Создание интерфейсов

---
+++ src/main/java/ru/job4j/ood/lsp/parking/Parking.java
@@ -0,0 +1,11 @@
+package ru.job4j.ood.lsp.parking;
+
+/**
+ * Интерфейс для реализации парковки.
+ *
+ * @author Andrey Kireenkov
+ * @version 1.0
+ * @since 14.09.2022
+ */
+public interface Parking {
+}
