9d7289ae61c35dcad2f64078cc5e9fcb985d98ff
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-09-16 14:24:55 +0300

    2. Парковка машин [#853 #343979]

--- src/main/java/ru/job4j/ood/lsp/parking/Parking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/Parking.java
@@ -1,5 +1,8 @@
 package ru.job4j.ood.lsp.parking;
 
+import ru.job4j.collection.list.List;
+import ru.job4j.ood.lsp.parking.car.Car;
+
 /**
  * Интерфейс для реализации парковки.
  *
@@ -8,4 +11,40 @@ package ru.job4j.ood.lsp.parking;
  * @since 14.09.2022
  */
 public interface Parking {
+    /**
+     * Метод определения типа автомобиля (легковой/грузовой).
+     *
+     * @param car объект типа Car.
+     * @return тип автомобиля passenger/truck.
+     */
+    String typeCar(Car car);
+
+    /**
+     * Метод добавления автомобиля на парковочное место.
+     *
+     * @param car объект типа Car.
+     * @return true, если автомобиль добавлен на парковку, иначе false.
+     */
+    boolean add(Car car);
+
+    /**
+     * Метод проверки, может ли автомобиль быть помещен на парковку.
+     * Проверяет наличие свободных парковочных мест, и сопоставляет с типом автомобиля.
+     *
+     * @param car объект типа Car.
+     * @return true, если автомобиль может быть добавлен на парковку, иначе false.
+     */
+    boolean accept(Car car);
+
+    /**
+     * Метод определяет количество оставшихся свободных мест на парковке.
+     *
+     * @return возвращает число свободных мест.
+     */
+    int numberOfAvailableParkingLots();
+
+    /**
+     * @return метод возвращает список автомобилей, которые помещены на парковку.
+     */
+    List<Car> getCarsList();
 }
